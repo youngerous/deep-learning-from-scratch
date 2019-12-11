82d4b6289d1d87d8339562cbf1d75e2cb90cd4d5
youngerous <youngerous@gmail.com>
2019-12-12 02:18:31 +0900

    ch04 loss func

--- common/functions.py
+++ common/functions.py
@@ -38,12 +38,29 @@ def softmax(x):
     x = x - np.max(x) # 오버플로 대책
     return np.exp(x) / np.sum(np.exp(x))
 
+########################################################################################
+## Q. 왜 정확도가 아닌 손실함수를 신경망 학습의 지표로 삼는가?                                      ##
+## A. 정확도를 지표로 하면 매개변수를 미분했을 때 대부분의 경우 0이 되기 때문이다.                      ##
+##    매개변수를 약간만 조정해서는 정확도가 개선되지 않고, 변화하더라도 불연속적인 값(discrete)으로 바뀐다.  ##
+########################################################################################
+
 
 def mean_squared_error(y, t):
+	"""
+	손실함수의 미분값을 구할 때 발생하는 미분계수(2)를 상쇄하기 위해 0.5를 곱한다.
+	"""
     return 0.5 * np.sum((y-t)**2)
 
 
 def cross_entropy_error(y, t):
+	"""
+	batch_size = 5일 때,
+	np.arange(batch_size) == [0, 1, 2, 3, 4]
+	t == [2, 7, 0, 9, 4] (label 예시)
+	
+	y[np.arange(batch_size), t] == [y[0,2], y[1,7], y[2,0], y[3,9], y[4,4]]
+		→ 각 데이터의 정답 레이블에 해당하는 신경망의 출력
+	""" 
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
@@ -53,7 +70,7 @@ def cross_entropy_error(y, t):
         t = t.argmax(axis=1)
              
     batch_size = y.shape[0]
-    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size # 마이너스 무한대 방지 위해 1e-7 더해줌.
 
 
 def softmax_loss(X, t):
