6a25c3112c9a9216ecb88e2950d1984dff9bcdda
youngerous <youngerous@gmail.com>
2020-02-06 15:38:11 +0900

    ch06 weight initialization

--- ch06/weight_init_activation_histogram.py
+++ ch06/weight_init_activation_histogram.py
@@ -28,8 +28,8 @@ for i in range(hidden_layer_size):
     # 초깃값을 다양하게 바꿔가며 실험해보자！
     w = np.random.randn(node_num, node_num) * 1
     # w = np.random.randn(node_num, node_num) * 0.01
-    # w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num)
-    # w = np.random.randn(node_num, node_num) * np.sqrt(2.0 / node_num)
+    # w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num) # Xavier initialization
+    # w = np.random.randn(node_num, node_num) * np.sqrt(2.0 / node_num) # He initialization
 
 
     a = np.dot(x, w)
@@ -51,3 +51,21 @@ for i, a in activations.items():
     # plt.ylim(0, 7000)
     plt.hist(a.flatten(), 30, range=(0,1))
 plt.show()
+
+"""
+	활성화값이 0과 1에 치우쳐 있는 경우 → Gradient vanishing
+	0.5 부근에 집중되어 있는 경우 → 표현력 관점에서 문제 (뉴런의 개수가 의미없어지는 경우)
+	
+	[대안] 
+	1. Xavier initialization (초깃값의 표준편차가 root(1/n)이 되도록 설정)
+		⇒ Activation function이 Linear함을 가정 (예: sigmoid, tanh)
+		⇒ ReLU에서 사용할 경우 layer가 깊어질수록 Gradient vanishing이 일어날 가능성이 높아짐
+	
+	2. He initialization (초깃값의 표준편차가 root(2/n)이 되도록 설정)
+		⇒ ReLU에 특화된 initialization
+			:음의 영역이 0이기 때문에 더 넓게 분포시키기 위함		
+			
+	[정리]
+	- Xavier init: sigmoid, tanh에서 사용
+	- He init    : ReLU에서 사용
+"""
